67e654acd473a5c40fa37ac17ccf774b51444be0
DavidMutchler <mutchler@rose-hulman.edu>
2018-06-03 17:52:23 -0400

    Added a module on Animation

--- prep/David/Day 1 - The Game Loop, Colors, Drawing and Animation/05-Animation.py
+++ prep/David/Day 1 - The Game Loop, Colors, Drawing and Animation/05-Animation.py
@@ -12,13 +12,82 @@ import sys
 pygame.init()
 screen = pygame.display.set_mode((640, 480))
 
+# while True:
+#     for event in pygame.event.get():
+#         print(event)
+#         if event.type == pygame.QUIT:
+#             sys.exit()
+#     screen.fill((255, 105, 180))  # Hot pink
+#     pygame.draw.circle(screen, (0, 0, 0), (100, 150), 20)
+#     pygame.draw.circle(screen, (255, 255, 0), (200, 350), 50)
+#     pygame.draw.rect(screen, (0, 0, 255), (20, 40, 100, 40), 5)
+#     pygame.display.update()
+
+
+# TODO: 7. Add animation per the code below (with just xpos, not yet xpo2).
+# TODO       Run, then discuss.  Then:
+# TODO         a. Students: Play with the animation speed.
+# TODO         b. Students: Animate MORE of the objects that you drew.
+# TODO              Be creative!
+
+# xpos = 50
+# xpos2 = 500
+# while True:
+#     for event in pygame.event.get():
+#         print(event)
+#         if event.type == pygame.QUIT:
+#             sys.exit()
+#     xpos = xpos + 1
+#     xpos2 = xpos2 = xpos2 - 5
+#     screen.fill((255, 105, 180))  # Hot pink
+#     pygame.draw.circle(screen, (0, 0, 0), (100, 150), 20)
+#     pygame.draw.circle(screen, (255, 255, 0), (xpos, 350), 50)
+#     pygame.draw.rect(screen, (0, 0, 255), (xpos2, 40, 100, 40), 5)
+#     pygame.display.update()
+
+
+# TODO: 8. Add a Clock to control the speed of the Game Loop,
+# TODO       per the code below.
+# TODO     Students:  Experiment with the clock speed.
+# TODO        What is the fastest speed your computer allows?
+# TODO        What happens as the speed gets slow?
+
+# xpos = 50
+# xpos2 = 500
+# clock = pygame.time.Clock()
+# while True:
+#     clock.tick(500)
+#     for event in pygame.event.get():
+#         print(event)
+#         if event.type == pygame.QUIT:
+#             sys.exit()
+#     xpos = xpos + 1
+#     xpos2 = xpos2 - 5
+#     screen.fill((255, 105, 180))  # Hot pink
+#     pygame.draw.circle(screen, (0, 0, 0), (100, 150), 20)
+#     pygame.draw.circle(screen, (255, 255, 0), (xpos, 350), 50)
+#     pygame.draw.rect(screen, (0, 0, 255), (xpos2, 40, 100, 40), 5)
+#     pygame.display.update()
+
+
+# TODO: 9. Add a caption for your program,
+# TODO       per the code below.
+# TODO     Students:  Choose your own caption.
+
+pygame.display.set_caption("My First Game")
+xpos = 50
+xpos2 = 500
+clock = pygame.time.Clock()
 while True:
+    clock.tick(500)
     for event in pygame.event.get():
         print(event)
         if event.type == pygame.QUIT:
             sys.exit()
+    xpos = xpos + 1
+    xpos2 = xpos2 - 5
     screen.fill((255, 105, 180))  # Hot pink
     pygame.draw.circle(screen, (0, 0, 0), (100, 150), 20)
-    pygame.draw.circle(screen, (255, 255, 0), (200, 350), 50)
-    pygame.draw.rect(screen, (0, 0, 255), (20, 40, 100, 40), 5)
+    pygame.draw.circle(screen, (255, 255, 0), (xpos, 350), 50)
+    pygame.draw.rect(screen, (0, 0, 255), (xpos2, 40, 100, 40), 5)
     pygame.display.update()
