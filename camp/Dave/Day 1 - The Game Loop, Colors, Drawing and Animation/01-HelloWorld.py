b7604986ffdd40a9c735eca54222616a100e4159
David Fisher <fisherds@rose-hulman.edu>
2018-06-04 16:00:24 -0400

    David Fisher day 1 additions

--- camp/Dave/Day 1 - The Game Loop, Colors, Drawing and Animation/01-HelloWorld.py
+++ camp/Dave/Day 1 - The Game Loop, Colors, Drawing and Animation/01-HelloWorld.py
@@ -1,13 +1,24 @@
 # Authors: David Mutchler, Dave Fisher, and many others before them.
 
-print('Hello, World')
-print('hi there')
+x = 9
+
+print("Hello, World")
+print('David Fisher can make Python programs!!!!')
 print('one', 'two', 'through my shoe')
 print(3 + 9)
 print('3 + 9', 'versus', 3 + 9)
 
-# TODO:  After we talk together about the above, add PRINT statements that print:
-# TODO:    1. A Hello message to a friend.
-# TODO:    2. Two big numbers, followed by their sum.
-# TODO:    3. The result of 3,607 multiplied by 34,227.  (Hint: the result is interesting.)
-# TODO:    4. Anything else you like!
+# DONE:  After we talk together about the above, add PRINT statements that print:
+# DONE:    1. A Hello message to a friend.
+print("Hello Seng, you smell very nice today!")
+
+# DONE:    2. Two big numbers, followed by their sum.
+print(305, 423, 305 + 423)
+print("305", "423", 305 + 423)
+print("305 + 423 = ", 305 + 423)
+
+# DONE:    3. The result of 3,607 multiplied by 34,227.  (Hint: the result is interesting.)
+print(3607 * 34227)
+
+# DONE:    4. Anything else you like!
+print("Hello " * 3)
