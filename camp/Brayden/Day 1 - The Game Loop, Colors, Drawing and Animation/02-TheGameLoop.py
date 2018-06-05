4027ee0387c07c12aa0dce6c47f61c5850390bcd
mobacdesign <fisherds@mobacdesign.com>
2018-06-05 16:10:17 -0400

    edit from tablet

--- camp/Brayden/Day 1 - The Game Loop, Colors, Drawing and Animation/02-TheGameLoop.py
+++ camp/Brayden/Day 1 - The Game Loop, Colors, Drawing and Animation/02-TheGameLoop.py
@@ -1,5 +1,7 @@
 # My first Pygame program.
-# Authors: Many people and <PUT-YOUR-NAME-HERE>
+# Authors: Many people and <Brayden>
+# My first Pygame program.
+# Authors: Many people and <Brayden>
 
 import pygame
 import sys
@@ -7,6 +9,26 @@ import sys
 pygame.init()
 screen = pygame.display.set_mode((640, 480))
 while True:
+    for event in pygame.event.get():
+        print(event)
+        if event.type == pygame.QUIT:
+            sys.exit()
+    pygame.display.update()
+
+
+# TODO: With your instructor's help (live coding):
+#   - Put your name as an author.
+#   - Make the game loop be able to stop more gracefully.
+import pygame
+import sys
+
+pygame.init()
+screen = pygame.display.set_mode((640, 480))
+while True:
+    for event in pygame.event.get():
+        print(event)
+        if event.type == pygame.QUIT:
+            sys.exit()
     pygame.display.update()
 
 
