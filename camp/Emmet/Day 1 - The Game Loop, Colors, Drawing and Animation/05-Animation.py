5d3880fb9eac9fdcfed4382fb7f56f62e7269096
mobacdesign <fisherds@mobacdesign.com>
2018-06-05 16:05:44 -0400

    edit from tablet

--- camp/Emmet/Day 1 - The Game Loop, Colors, Drawing and Animation/05-Animation.py
+++ camp/Emmet/Day 1 - The Game Loop, Colors, Drawing and Animation/05-Animation.py
@@ -1,1 +1,38 @@
 # TODO: Copy all of your   04-Drawing.py   program and put it below this comment.
+# TODO: Copy all of your   03-Colors.py   program and put it below this comment.
+# TODO    One way to do so is:
+# TODO      1. Inside  03-Colors.py,  do:
+# TODO           -- Control-A (to SELECT the entire contents of the file, then
+# TODO           -- Control-C (to COPY that entire selection)
+# TODO      2. Inside this file:
+# TODO           -- Click below this comment, then
+# TODO           -- Control-V (to PASTE the copied code into this file.
+# TODO: Copy all of your   02-TheGameLoop.py   program and put it below this comment.
+# TODO    One way to do so is:
+# TODO      1. Inside  02-TheGameLoop.py,  do:
+# TODO           -- Control-A (to SELECT the entire contents of the file, then
+# TODO           -- Control-C (to COPY that entire selection)
+# TODO      2. Inside this file:
+# TODO           -- Click below this comment, then
+# TODO           -- Control-V (to PASTE the copied code into this file.
+# My first Pygame program.
+# Authors: Many people and <emmett>
+
+import pygame
+import sys
+xpos = 1
+screen = pygame.display.set_mode((640, 480))
+while True:
+    for event in pygame.event.get():
+        print(event)
+        if event.type == pygame.QUIT:
+            sys.exit()
+    xpos = xpos + 9999999
+    screen.fill((244, 105, 180))  # Hot pink
+    pygame.draw.circle(screen, (255, 255, 255,), (xpos, 255), 255)
+    pygame.display.update()
+
+
+# TODO: With your instructor's help (live coding):
+#   - Put your name as an author.
+#   - Make the game loop be able to stop more gracefully.
