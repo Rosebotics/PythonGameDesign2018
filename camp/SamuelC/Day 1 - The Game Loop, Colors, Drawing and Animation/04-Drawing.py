36cfacf2b8a12a10d3be8e974fc922659f27f31e
David Fisher <rosehulmanfisher@gmail.com>
2018-06-05 14:54:48 -0400

    Samuel Carson Day 1

--- camp/SamuelC/Day 1 - The Game Loop, Colors, Drawing and Animation/04-Drawing.py
+++ camp/SamuelC/Day 1 - The Game Loop, Colors, Drawing and Animation/04-Drawing.py
@@ -5,4 +5,29 @@
 # TODO           -- Control-C (to COPY that entire selection)
 # TODO      2. Inside this file:
 # TODO           -- Click below this comment, then
-# TODO           -- Control-V (to PASTE the copied code into this file.
+# TODO           -- Control-V (to PASTE the copied coimport pygame
+
+import pygame
+import sys
+pygame.init()
+pygame.display.set_caption("drawing")
+screen = pygame.display.set_mode((640, 480))
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+    screen.fill((234, 162, 223))
+
+    #drawing shapes on the screen
+    #big circle
+    pygame.draw.circle(screen, (0, 125, 255), (320, 240), 200, 200)
+    #left eye
+    pygame.draw.circle(screen, (0, 255, 255), (240, 160), 66, 40)
+    #rtight eye
+    pygame.draw.circle(screen, (0, 255, 0), (400, 160), 66, 40)
+    #nose
+    pygame.draw.circle(screen, (255, 0, 0), (320, 240), 40, 20)
+    #muoth
+    pygame.draw.rect(screen, (0, 255, 255), (200,320,240,30))
+
+    pygame.display.update()
