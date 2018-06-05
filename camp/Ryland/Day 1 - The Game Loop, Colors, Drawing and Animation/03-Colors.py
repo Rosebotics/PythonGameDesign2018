a896b4bb0be2f659617c85bc65656bb76044db29
rhgitldap <rhgitldap@rose-hulman.edu>
2018-06-05 13:37:19 -0400

    Added a caption

--- camp/Ryland/Day 1 - The Game Loop, Colors, Drawing and Animation/03-Colors.py
+++ camp/Ryland/Day 1 - The Game Loop, Colors, Drawing and Animation/03-Colors.py
@@ -1,20 +1,23 @@
 
 
 
-# My first Pygame program.
+# My first Pygame program. i like turtles
 # Authors: Many people and <Ryland Hayes 62 127 255>
 
 import pygame
 import sys
 
+
 pygame.init()
+pygame.display.set_caption("Ryland Hayes is Awesome")
+pygame.display.set_mode((640, 480))
 screen = pygame.display.set_mode((640, 480))
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-    screen.fill( ( 0, 0,0) )
+    screen.fill( ( 65, 125,255) )
     pygame.display.update()
-
+print('Hello World')
 
 
