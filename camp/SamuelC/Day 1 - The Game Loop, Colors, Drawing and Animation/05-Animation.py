09ffc2529ccdc3c80f6f0dfb1a4325760472b95c
David Fisher <rosehulmanfisher@gmail.com>
2018-06-05 15:34:01 -0400

    Samuel Carson Day 1

--- camp/SamuelC/Day 1 - The Game Loop, Colors, Drawing and Animation/05-Animation.py
+++ camp/SamuelC/Day 1 - The Game Loop, Colors, Drawing and Animation/05-Animation.py
@@ -2,9 +2,15 @@
 import pygame
 import sys
 pygame.init()
-pygame.display.set_caption("drawing")
+pygame.display.set_caption("animation")
 screen = pygame.display.set_mode((640, 480))
+
+clock = pygame.time.Clock()
+
+nose_y_position = 240
+
 while True:
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
@@ -18,8 +24,11 @@ while True:
     #rtight eye
     pygame.draw.circle(screen, (0, 255, 0), (400, 160), 66, 40)
     #nose
-    pygame.draw.circle(screen, (255, 0, 0), (320, 240), 40, 20)
+    nose_y_position = nose_y_position + 122
+    if nose_y_position > 400:
+        nose_y_position = 200
+    pygame.draw.circle(screen, (255, 0, 0), (320, nose_y_position), 40, 20)
     #muoth
-    pygame.draw.rect(screen, (0, 255, 255), (200,320,240,30))
+    pygame.draw.rect(screen, (0, 255, 255), (200, 320, 240, 30))
 
     pygame.display.update()
