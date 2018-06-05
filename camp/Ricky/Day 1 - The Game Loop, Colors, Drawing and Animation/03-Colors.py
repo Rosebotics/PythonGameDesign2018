e271d35c000f3eab9d4cb3c4734ec618473f3d0d
David Fisher <rosehulmanfisher@gmail.com>
2018-06-05 12:44:22 -0400

    Ricky lee day 1 additions

--- camp/Ricky/Day 1 - The Game Loop, Colors, Drawing and Animation/03-Colors.py
+++ camp/Ricky/Day 1 - The Game Loop, Colors, Drawing and Animation/03-Colors.py
@@ -6,3 +6,16 @@
 # TODO      2. Inside this file:
 # TODO           -- Click below this comment, then
 # TODO           -- Control-V (to PASTE the copied code into this file.
+
+import pygame
+import sys
+
+pygame.init()
+screen = pygame.display.set_mode((640, 480))
+while True:
+    for event in pygame.event.get():
+        print(event)
+        if event.type == pygame.QUIT:
+            sys.exit()
+    screen.fill((124, 68, 173))
+    pygame.display.update()
