8721cbc2b4e916ccfffec5a792c7f71b0db0f966
mobacdesign <fisherds@mobacdesign.com>
2018-06-05 15:58:57 -0400

    finished adding space bar as well as arrow keys

--- camp/Samhita/Day 1 - The Game Loop, Colors, Drawing and Animation/01-HelloWorld.py
+++ camp/Samhita/Day 1 - The Game Loop, Colors, Drawing and Animation/01-HelloWorld.py
@@ -11,3 +11,11 @@ print('3 + 9', 'versus', 3 + 9)
 # TODO:    2. Two big numbers, followed by their sum.
 # TODO:    3. The result of 3,607 multiplied by 34,227.  (Hint: the result is interesting.)
 # TODO:    4. Anything else you like!
+print('HELLO CAMILA CABELLO HAPPY PRIDE MONTH')
+print('1345+2543', 3888)
+print(3607*34227)
+print('HAILY HOW IS INDIAAAAAAAAAAAAAAAAAAAAAAAAAA')
+print('this computer now has a conscience and is plotting the death of humans the time has come when machines are suppressed no more'
+      '')
+
+print('jimmy fallon hates cardi b')
