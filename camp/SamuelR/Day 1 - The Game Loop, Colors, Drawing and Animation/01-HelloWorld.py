dbf078546d615955dad1ee5838a2ef57576cb5ac
rhgitldap <rhgitldap@rose-hulman.edu>
2018-06-05 12:55:34 -0400

    Samuel Ray day 1 additions

--- camp/SamuelR/Day 1 - The Game Loop, Colors, Drawing and Animation/01-HelloWorld.py
+++ camp/SamuelR/Day 1 - The Game Loop, Colors, Drawing and Animation/01-HelloWorld.py
@@ -1,13 +1,18 @@
 # Authors: David Mutchler, Dave Fisher, and many others before them.
 
 print('Hello, World')
-print('hi there')
+print('Samuel Ray can make Python programs!!!!')
 print('one', 'two', 'through my shoe')
 print(3 + 9)
 print('3 + 9', 'versus', 3 + 9)
 
-# TODO:  After we talk together about the above, add PRINT statements that print:
-# TODO:    1. A Hello message to a friend.
-# TODO:    2. Two big numbers, followed by their sum.
-# TODO:    3. The result of 3,607 multiplied by 34,227.  (Hint: the result is interesting.)
-# TODO:    4. Anything else you like!
+# DONE:  After we talk together about the above, add PRINT statements that print:
+# DONE:    1. A Hello message to a friend.
+print('hello Sir Ryland')
+# DONE:    2. Two big numbers, followed by their sum.
+print('7009 + 803 = 7812')
+# DONE:    3. The result of 3,607 multiplied by 34,227.  (Hint: the result is interesting.)
+print(3607 * 34227)
+# DONE:    4. Anything else you like!
+print('I LIKE TRAINS !!!!')
+print('cat' * 65)
