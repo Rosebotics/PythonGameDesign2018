86cc741c95c990e5d03fad4b43058fa38b81c911
David Fisher <rosehulmanfisher@gmail.com>
2018-06-05 15:40:36 -0400

    Samuel Carson Day 1

--- camp/SamuelC/Day 1 - The Game Loop, Colors, Drawing and Animation/05-Animation.py
+++ camp/SamuelC/Day 1 - The Game Loop, Colors, Drawing and Animation/05-Animation.py
@@ -1,6 +1,7 @@
-# TODO: Copy all of your   04-Drawing.py   program and put it below this comment.
+
 import pygame
 import sys
+
 pygame.init()
 pygame.display.set_caption("animation")
 screen = pygame.display.set_mode((640, 480))
@@ -16,19 +17,19 @@ while True:
             sys.exit()
     screen.fill((234, 162, 223))
 
-    #drawing shapes on the screen
-    #big circle
+    # drawing shapes on the screen
+    # big circle
     pygame.draw.circle(screen, (0, 125, 255), (320, 240), 200, 200)
-    #left eye
+    # left eye
     pygame.draw.circle(screen, (0, 255, 255), (240, 160), 66, 40)
-    #rtight eye
+    # rtight eye
     pygame.draw.circle(screen, (0, 255, 0), (400, 160), 66, 40)
-    #nose
+    # nose
     nose_y_position = nose_y_position + 122
     if nose_y_position > 400:
         nose_y_position = 200
     pygame.draw.circle(screen, (255, 0, 0), (320, nose_y_position), 40, 20)
-    #muoth
+    # muoth
     pygame.draw.rect(screen, (0, 255, 255), (200, 320, 240, 30))
 
     pygame.display.update()
