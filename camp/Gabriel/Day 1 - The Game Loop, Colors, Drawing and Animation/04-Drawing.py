9b579b2b10abeac468fb322b57b2477e7b07f45a
mobacdesign <fisherds@mobacdesign.com>
2018-06-08 16:06:57 -0400

    Merge branch 'master' of /home/csse/PyCharmProjets/github/PythonGameDesign2018 with conflicts.

--- camp/Gabriel/Day 1 - The Game Loop, Colors, Drawing and Animation/04-Drawing.py
+++ camp/Gabriel/Day 1 - The Game Loop, Colors, Drawing and Animation/04-Drawing.py
@@ -11,18 +11,16 @@ import sys
 
 pygame.init()
 pygame.display.set_caption("My first program")
-screen = pygame.display.set_mode((1080, 720))
+screen = pygame.display.set_mode((640, 480))
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
     screen.fill((234,123 , 245))
 
-    pygame.draw.circle(screen, (255,255,0), (320,240), 210)
-    pygame.draw.circle(screen, (0,0,0), (240,160 ),25)
-    pygame.draw.circle(screen, (0, 0, 0), (400,160),25)
-    pygame.draw.circle(screen, (0, 0, 0),(320,240),20,5 )
 
-    pygame.draw.rect(screen, (100,0,0),     (200, 320, 240, 30))
-
+    pygame.draw.circle(screen, (255,255,0), )
+    pygame.draw.circle(screen, (0,0,0), )
+    pygame.draw.circle(screen, (0, 0, 0), )
+    pygame.draw.circle(screen, (0, 0, 0), )
     pygame.display.update()
