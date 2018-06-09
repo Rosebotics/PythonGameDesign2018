1b2ac03b530d55d3170e630df2d5f06ccfdfd1d2
rhgitldap <rhgitldap@rose-hulman.edu>
2018-06-09 11:22:19 -0400

    added a caption to the  window

--- camp/JacksonC/Day 1 - The Game Loop, Colors, Drawing and Animation/06-InteractingWithAPlayer.py
+++ camp/JacksonC/Day 1 - The Game Loop, Colors, Drawing and Animation/06-InteractingWithAPlayer.py
@@ -12,7 +12,7 @@ nose_y_position = 240
 
 while True:
      pressed_keys = pygame.key.get_pressed()
-     if pressed_keys = pygame.K_RIGHT]:
+     if pressed_keys = [pygame.K_RIGHT]:
          nose_y_position = 0
      clock.tick(9090909090909)
      for event in pygame.event.get():
